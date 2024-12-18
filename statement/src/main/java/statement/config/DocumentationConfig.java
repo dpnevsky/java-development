d7a3d9dce9ba76c1b98aaad80e29ec955a687fbe
dpnevsky <dpnevsky@gmail.com>
2024-12-18 04:12:40 +0300

    init app statement

---
+++ statement/src/main/java/statement/config/DocumentationConfig.java
@@ -0,0 +1,18 @@
+package statement.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DocumentationConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Statement API")
+                        .version("1.0")
+                        .description("API for prescoring the statement, saving it to MC Deal and sending it to the MC calculator."));
+    }
+}
